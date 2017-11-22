8810b9cc489206f068d5fda553900853e147fbb8
lorsi <lorsi77@gmail.com>
2017-11-22 10:15:45 +0100

    cambiamos nombre

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 <article>
     <div class="contenido">
         <h1>Principal</h1>
-        <p>Bienvenido a la Guía Llerena!!!</p>
+        <p>Bienvenido a la Guía Zafra!!!</p>
         <p>En esta guía encontraras cualquier empresas de Llerena para poderte comunicar con ellos y recibir sus servicios.</p>
 <p>En nuestras redes sociales comunicaremos novedades de las empresas para que estéis informados de todas las oportunidades que nos ofrecen las empresas de Llerena.
 </p>
+
     </div>
 </article>
 
